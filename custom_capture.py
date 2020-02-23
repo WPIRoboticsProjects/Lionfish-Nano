af34006bb91f8d5c37cec760f1c3bca0e94af9a2
Nicholas <nbjohnson10@gmail.com>
2020-02-23 13:23:40 -0500

    try antother capture type

--- custom_capture.py
+++ custom_capture.py
@@ -5,7 +5,7 @@ import threading
 def cam_thread(cam):
     while True:
         val, img = cam.read()
-        cv.imshow("cam", img)
+        cv.imshow("cam", cv.pyrDown(img))
         if cv.waitKey(1) == 27:
             break
 
@@ -16,7 +16,7 @@ camera = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.1:1.0-video-index0"
 cam = cv.VideoCapture(camera)
 print("Camera: " + str(cam.isOpened()))
 
-x = threading.Thread(target=cam_thread, args=(1, cam))
+x = threading.Thread(target=cam_thread, args=[cam])
 x.start()
 while counter < num_photos:
     input("Next image: ")

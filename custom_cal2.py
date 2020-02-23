1ed6959adaf12f95c52cde22516e5ca5271f171c
Nicholas <nbjohnson10@gmail.com>
2020-02-23 13:37:36 -0500

    fix cust cal

--- custom_cal2.py
+++ custom_cal2.py
@@ -8,8 +8,8 @@ left_device = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.1:1.0-video-index
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+objp = np.zeros((9*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -22,7 +22,7 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+    ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -32,7 +32,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv.drawChessboardCorners(img, (7,6), corners2,ret)
+        img = cv.drawChessboardCorners(img, (9,6), corners2,ret)
         # cv.imshow('img', img)
         cv.waitKey(500)
 

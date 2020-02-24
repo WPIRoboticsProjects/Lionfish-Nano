c5005450cb2f8403bc956bff68b1b3e7ce3c0b50
Nicholas <nbjohnson10@gmail.com>
2020-02-24 17:01:42 -0500

    try another stereo vis

--- custom_capture.py
+++ custom_capture.py
@@ -2,29 +2,37 @@ import cv2 as cv
 import threading
 
 
-def cam_thread(cam):
+def cam_thread(cam1, cam2):
     while True:
-        val, img = cam.read()
-        cv.imshow("cam", cv.pyrDown(img))
+        val1, img1 = cam1.read()
+        val2, img2 = cam2.read()
+        cv.imshow("cam1", cv.pyrDown(img1))
+        cv.imshow("cam2", cv.pyrDown(img2))
         if cv.waitKey(1) == 27:
             break
 
 counter = 0
 num_photos = 5
-# camera = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.1:1.0-video-index0"
-camera = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.3:1.0-video-index0"
+camera1 = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.1:1.0-video-index0"
+camera2 = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.3:1.0-video-index0"
 
-cam = cv.VideoCapture(camera)
-print("Camera: " + str(cam.isOpened()))
+cam1 = cv.VideoCapture(camera1)
+cam2 = cv.VideoCapture(camera2)
+print("Camera: " + str(cam1.isOpened()))
+print("Camera: " + str(cam2.isOpened()))
 
-x = threading.Thread(target=cam_thread, args=[cam])
+x = threading.Thread(target=cam_thread, args=[cam1, cam2])
 x.start()
 while counter < num_photos:
     input("Next image: ")
-    val, img = cam.read()
-    name = "./calibration_photos_water/right/" + str(counter) + ".png"
-    cv.imwrite(name, img)
+    val1, img1 = cam1.read()
+    val2, img2 = cam2.read()
+    name1 = "calibration_photos_stereo_test/left/" + str(counter) + ".png"
+    name2 = "calibration_photos_stereo_test/right/" + str(counter) + ".png"
+    cv.imwrite(name1, img1)
+    cv.imwrite(name2, img2)
     print("Image " + str(counter) + " saved")
     counter += 1
 
-cam.release()
+cam1.release()
+cam2.release()
---
+++ custom_stereo2.py
@@ -0,0 +1,130 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+left_device = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.1:1.0-video-index0"
+right_device = "/dev/v4l/by-path/platform-70090000.xusb-usb-0:2.3:1.0-video-index0"
+
+left = cv.VideoCapture(left_device)
+right = cv.VideoCapture(right_device)
+
+print("Left: " + str(left.isOpened()))
+print("Right: " + str(right.isOpened()))
+
+#---------------------------------------------------
+# Calibration
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((9*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+objpoints = [] # 3d point in real world space
+imgpoints_l = [] # 2d points in image plane.
+imgpoints_r = [] # 2d points in image plane.
+
+images_l = glob.glob('calibration_photos_stereo_test/left/*.png')
+images_r = glob.glob('calibration_photos_stereo_test/right/*.png')
+
+for i in range(0, len(images_l)):
+    img_l = cv.imread(images_l.index(i))
+    img_r = cv.imread(images_r.index(i))
+    gray_l = cv.cvtColor(img_l, cv.COLOR_BGR2GRAY)
+    gray_r = cv.cvtColor(img_r, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret_l, corners_l = cv.findChessboardCorners(gray_l, (9, 6), None)
+    ret_r, corners_r = cv.findChessboardCorners(gray_r, (9, 6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret_l and ret_r:
+        objpoints.append(objp)
+
+        corners2_l = cv.cornerSubPix(gray_l, corners_l, (11, 11), (-1, -1), criteria)
+        corners2_r = cv.cornerSubPix(gray_r, corners_r, (11, 11), (-1, -1), criteria)
+        imgpoints_l.append(corners2_l)
+        imgpoints_r.append(corners2_r)
+
+        # Draw and display the corners
+        img_l_show = cv.drawChessboardCorners(img_l, (9,6), corners2_l,ret_l)
+        img_r_show = cv.drawChessboardCorners(img_r, (9, 6), corners2_r, ret_r)
+        cv.imshow('img_l', img_l_show)
+        cv.imshow('img_r', img_r_show)
+        cv.waitKey(500)
+
+cv.destroyAllWindows()
+
+
+cameraMatrixLeft = np.zeros( (3,3) )
+cameraMatrixRight = np.zeros( (3,3) )
+distortionLeft = np.zeros( (8,1) )
+distortionRight = np.zeros( (8,1) )
+height, width = cv.pyrDown(img_l).shape[:2]
+
+rms, leftMatrix, leftDistortion, rightMatrix, rightDistortion, R, T, E, F = cv.stereoCalibrate(objpoints, imgpoints_l, imgpoints_r,  cameraMatrixLeft, distortionLeft, cameraMatrixRight, distortionRight, (width, height),criteria, flags=0)
+
+leftRectTransform, rightRectTransform, leftProjMatrix, rightProjMatrix, _, _, _ = cv.stereoRectify(leftMatrix, leftDistortion, rightMatrix, rightDistortion,  (width, height), R, T, alpha=-1)
+leftMapX, leftMapY = cv.initUndistortRectifyMap(leftMatrix, leftDistortion, leftRectTransform, leftProjMatrix, (width, height), cv.CV_32FC1)
+rightMapX, rightMapY = cv.initUndistortRectifyMap(rightMatrix, rightDistortion, rightRectTransform, rightProjMatrix, (width, height), cv.CV_32FC1)
+
+#----------------------------------------------------
+# show disparity
+
+
+#window_size = 25#4
+#min_disp = 0#16 #32#-64 #16
+#num_disp = 112-min_disp
+#num_disp = 128-min_disp
+#num_disp = 144-min_disp
+#stereo = cv.StereoSGBM_create(
+#    minDisparity = 0, #min_disp
+#    numDisparities = 96, #128, #num_disp,
+#    blockSize = 5, #1, #5, #7, #9, # 16
+#    P1 = 0, #8*3*window_size**2,
+#    P2 = 0, #960, #32*3*window_size**2,
+#    disp12MaxDiff = 83, #30, #7, #10, #1,
+#    uniquenessRatio = 0, #10,
+#    speckleWindowSize = 0, #100,
+#    speckleRange = 0 #9 #32
+#)
+
+
+stereo = cv.StereoSGBM_create(
+    minDisparity = 0, #min_disp
+    numDisparities = 96, #128, #num_disp,
+    blockSize = 1, #1, #5, #7, #9, # 16
+    P1 = 100, #8*3*window_size**2,
+    P2 = 800, #960, #32*3*window_size**2,
+    disp12MaxDiff = 60, #83, #30, #7, #10, #1,
+    uniquenessRatio = 0, #3, #10,
+    speckleWindowSize = 0, #100,
+    speckleRange = 0 #9 #32
+)
+
+
+while True:
+    val, l_img = left.read()
+    val2, r_img = right.read()
+    if cv.waitKey(1) == 27:
+        break
+    imgL = cv.pyrDown(l_img)
+    imgR = cv.pyrDown(r_img)
+
+    frameLeftNew = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
+    frameRightNew = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
+
+    leftRectified = cv.remap(frameLeftNew, leftMapX, leftMapY, cv.INTER_LINEAR)
+    rightRectified = cv.remap(frameRightNew, rightMapX, rightMapY, cv.INTER_LINEAR)
+
+    disparity = stereo.compute(leftRectified, rightRectified).astype(np.float32) / 16.0
+
+    # cv.filterSpeckles(disparity, 0, 6000, 96)
+    # cv.imshow("Normalized Disparity", (disparity / 16.0 - 0) / 96);
+
+
+    # cv.imshow("Normalized Disparity", (disparity / 16.0 - 0) / 128)
+    cv.imshow("Normalized Disparity", (disparity - 0) / 128)
+
+
+
+left.release()
+right.release()

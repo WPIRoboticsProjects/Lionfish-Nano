51bc1e8ff916e291503c841a36c5988508bff982
Harrison Saperstein <hksaperstrein@wpi.edu>
2020-03-03 16:16:50 -0500

    arduino queues

--- process/classes/ProcessQueue.py
+++ process/classes/ProcessQueue.py
@@ -7,9 +7,11 @@ for all processed to access all needed queues
 '''
 class ProcessQueue:
     def __init__(self):
-        self.sensor_data = Queue()# 0-ping1 1- ping1conf 2- ping2 3- ping3conf
+        # self.sensor_data = Queue()# 0-ping1 1- ping1conf 2- ping2 3- ping3conf
         self.mavlink_nav = Queue()
         self.mavlink_depth = Queue()
+        self.arduino_nav = Queue()
+        self.arduino_depth = Queue()
         self.ui_nav = Queue()
         self.ui_depth = Queue()
 

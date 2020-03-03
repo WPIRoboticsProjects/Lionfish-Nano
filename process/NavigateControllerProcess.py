ae681d82be35e14b2bbf25e46e87a0483a188793
Nicholas <nbjohnson10@gmail.com>
2020-03-03 15:51:16 -0500

    pull mavlink data from queue

--- process/NavigateControllerProcess.py
+++ process/NavigateControllerProcess.py
@@ -20,15 +20,19 @@ class NavigateControllerProcess(Process):
         direction = 0
         desired_amount = 0
         roomba_state = 'straight'
+
+        while self.__queues.mavlink_nav.qsize() == 0:
+            pass
+        mavlink_data = self.__queues.mavlink_nav.get()
+
         while True:
 
             # todo: add in no wait if empty queues (prob in ProcessQueues class)
             new_message = self.__queues.ui_nav.get()
             sensor_data = self.__queues.sensor_data.get()
 
-            # todo: probably better to make a process that pulls mavlink data
-            #  and parses it into message queues
-            mavlink_data = self.__mavlink.recv_match()
+            if self.__queues.mavlink_nav.qsize() != 0:
+                mavlink_data = self.__queues.mavlink_nav.get()
 
             # todo: make better standard messages for cmd passing and such,
             #  tuples for now
@@ -38,12 +42,7 @@ class NavigateControllerProcess(Process):
                 direction = new_message[2]
                 desired_amount = new_message[3]  # time or angle
 
-                if not mavlink_data:
-                    pass
-                elif mavlink_data.get_type() == 'VFR_HUD':
-                    original_heading = mavlink_data.to_dict()['heading']
-                else:
-                    pass
+                original_heading = mavlink_data
 
                 start_time = time.time()
 
@@ -58,12 +57,7 @@ class NavigateControllerProcess(Process):
                     state = 'stop'
 
             elif state == 'turn':
-                if not mavlink_data:
-                    continue
-                elif mavlink_data.get_type() == 'VFR_HUD':
-                    current_heading = mavlink_data.to_dict()['heading']
-                else:
-                    continue
+                current_heading = mavlink_data
                 desired_rel_angle = direction * desired_amount
                 if self.__nav_obj.continue_turn(original_heading, current_heading, desired_rel_angle):
                     self.__nav_obj.turn(throttle, desired_rel_angle)
@@ -80,12 +74,7 @@ class NavigateControllerProcess(Process):
                         roomba_state = 'turn'
 
                 elif roomba_state == 'turn':
-                    if not mavlink_data:
-                        continue
-                    elif mavlink_data.get_type() == 'VFR_HUD':
-                        current_heading = mavlink_data.to_dict()['heading']
-                    else:
-                        continue
+                    current_heading = mavlink_data
 
                     if(self.__nav_obj.continue_turn(original_heading, current_heading, 100)):
                         self.__nav_obj.turn(throttle, desired_rel_angle)

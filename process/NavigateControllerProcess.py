aab196dac50475a2b6c64c3f81347b2c32403ca5
Harrison Saperstein <hksaperstrein@wpi.edu>
2020-03-05 18:51:48 -0500

    message

--- process/NavigateControllerProcess.py
+++ process/NavigateControllerProcess.py
@@ -75,7 +75,7 @@ class NavigateControllerProcess(Process):
                 current_heading = mavlink_data
                 # print(current_heading)
                 desired_rel_angle = direction * desired_amount
-                if self.nav_obj.is_turn_finished(current_heading, desired_rel_angle):
+                if self.nav_obj.is_turn_finished(original_heading, current_heading, desired_rel_angle):
                     self.nav_obj.turn(throttle, desired_rel_angle)
                 else:
                     last_message = new_message
